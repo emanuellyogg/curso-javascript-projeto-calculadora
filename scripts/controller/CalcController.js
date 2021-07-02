ad7d25de6ef49a830b3c69e616d1acf96d1b00af
unknown <manuogg90@gmail.com>
2021-07-02 11:29:28 -0300

    feat: uso de get e set

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,22 @@
 class CalcController {
+  constructor() {
+    this._displayCalc = '0';
+    this._dataAtual;
+  }
 
+  get displayCalc() {
+    return this.displayCalc;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalc = valor;
+  }
+
+  get dataAtual() {
+    return this.dataAtual;
+  }
+
+  set dataAtual(valor) {
+    this._dataAtual = valor;
+  }
 }
